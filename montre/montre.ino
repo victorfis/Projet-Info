dc2ee587aafd644007e261746587bff66923287c
chenmuyao <chenmuyao95@gmail.com>
2017-03-02 17:53:40 +0100

    Avec Bluetooth!

--- montre/montre.ino
+++ montre/montre.ino
@@ -1,19 +1,24 @@
 /* 
- * example code illustrating Time l.
+ * TimeSerial.pde
+ * example code illustrating Time library set through serial port messages.
+ *
  * Messages consist of the letter T followed by ten digit time (as seconds since Jan 1 1970)
  * you can send the text on the next line using Serial Monitor to set the clock to noon Jan 1 2013
  T1357041600  
  *
  * A Processing example sketch to automatically send the messages is included in the download
  * On Linux, you can use "date +T%s\n > /dev/ttyACM0" (UTC time zone)
- * Or echo -e "$(date +T%s)\n" > /dev/ttyACM0
+ * echo -e "$(date +T%s)\n" > /dev/ttyACM0
  */ 
  
 #include <TimeLib.h>
-#include <Adafruit_ssd1306syp.h> //This is a simplified library
+#include <Adafruit_ssd1306syp.h>
+#include <SoftwareSerial.h>
 
-#define SDA_PIN A4  //SDA pin of OLED connected to A4
-#define SCL_PIN A5  //SCL pin of OLED connected to A5
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+#define SDA_PIN A4
+#define SCL_PIN A5
 
 Adafruit_ssd1306syp display(SDA_PIN,SCL_PIN);
 
@@ -21,23 +26,27 @@ Adafruit_ssd1306syp display(SDA_PIN,SCL_PIN);
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
 void setup()  {
-  Serial.begin(9600);
-  while (!Serial) ; // Needed for Leonardo only
+  //Serial.begin(9600);
+  //while (!Serial) ; // Needed for Leonardo only
+  
+  mySerial.begin(38400);
+  
   pinMode(13, OUTPUT);
   setSyncProvider( requestSync);  //set function to call when sync required
-  Serial.println("Waiting for sync message");
+  mySerial.println("Waiting for sync message");
   delay(1000);
-  //Initialization of OLED
-  display.initialize(); 
+  display.initialize();
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(10,20);
   display.print("WATCH'INT");
   display.update();
+
+
 }
 
 void loop(){    
-  if (Serial.available()) {
+  if (mySerial.available()) {
     processSyncMessage();
   }
   if (timeStatus()!= timeNotSet) {
@@ -56,11 +65,13 @@ void digitalClockDisplay(){
   // digital clock display of the time
   display.setTextSize(2);
   display.setTextColor(WHITE);
-  display.setCursor(10,20);
+  display.setCursor(15,20);
   display.print(hour());
   printDigits(minute());
   printDigits(second());
-  display.println(); 
+  display.setCursor(15,40);
+ 
+  //display.print(" ");
   display.print(day());
   display.print("/");
   display.print(month());
@@ -82,8 +93,8 @@ void processSyncMessage() {
   unsigned long pctime;
   const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
 
-  if(Serial.find(TIME_HEADER)) {
-     pctime = Serial.parseInt();
+  if(mySerial.find(TIME_HEADER)) {
+     pctime = mySerial.parseInt();
      if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
        setTime(pctime); // Sync Arduino clock to the time received on the serial port
      }
@@ -92,7 +103,8 @@ void processSyncMessage() {
 
 time_t requestSync()
 {
-  Serial.write(TIME_REQUEST);  
+  mySerial.write(TIME_REQUEST);  
   return 0; // the time will be sent later in response to serial mesg
 }
+
 
